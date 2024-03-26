5ee9599b1c10d640ffdb79a1ced5d3b2c76a9b87
himanshudash132 <Himanshudash10@gmail.com>
2024-03-26 10:49:11 +0530

    new

--- app.py
+++ app.py
@@ -56,3 +56,67 @@ if choise == "Download":
    with open('best_model.pkl','rb') as f:      
         st.download_button('Download Model',f,file_name="best_model.pkl")
 
+
+
+   
+# from operator import index  # Importing index from operator module (unused in provided code)
+# import streamlit as st  # Importing Streamlit library and aliasing it as st
+# import plotly.express as px  # Importing Plotly Express library and aliasing it as px
+# import pandas as pd  # Importing pandas library and aliasing it as pd
+# import os  # Importing os module for interacting with the operating system
+
+# # streamlit run app.py
+
+# # Importing profiling libraries
+# # from pandas_profiling import ProfileReport  (Commented out, not used in provided code)
+# from streamlit_pandas_profiling import st_profile_report  # Importing Streamlit Pandas Profiling module
+# from ydata_profiling import ProfileReport  # Importing ydata-profiling module
+
+# # Importing ML modules
+# from pycaret.classification import setup, compare_models, pull, save_model, load_model  # Importing PyCaret classification functions
+
+# # Setting up Streamlit sidebar
+# with st.sidebar:
+#      st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")  # Displaying an image in the sidebar
+#      st.title("</> AutoStreamML")  # Setting the title in the sidebar
+#      choise = st.radio("Navigation",["Upload","Profiling","Modelling","Download"])  # Creating a radio button group for navigation
+#      st.info("Go ahead knock yourself out guys, do whatever you want")  # Displaying an info message in the sidebar
+#      st.info("This application allows you to build an automated ML pipeline using Streamlit, ydata-profiling and PyCaret")  # Displaying an info message in the sidebar
+
+# # Checking if the file 'sourcedata.csv' exists
+# if os.path.exists('sourcedata.csv'):
+#    df = pd.read_csv('sourcedata.csv',index_col=None)  # Reading the CSV file into a DataFrame
+
+# # Handling different choices made by the user
+# if choise == "Upload":
+#     st.title("Upload Your Dataset")  # Setting the title for the upload section
+#     file = st.file_uploader("Upload Your Dataset")  # Creating a file uploader widget
+#     if file:
+#         df = pd.read_csv(file,index_col=None)  # Reading the uploaded file into a DataFrame
+#         df.to_csv('sourcedata.csv', index=None)  # Saving the DataFrame to a CSV file
+#         st.dataframe(df)  # Displaying the DataFrame in the app
+
+# if choise == "Profiling":
+#     st.title("Automated Exploratory Data Analysis")  # Setting the title for the profiling section
+#     st.write("Read Documentation ---> https://docs.profiling.ydata.ai/latest/")  # Providing a link to documentation
+#     profile_df = df.profile_report()  # Generating a profile report for the DataFrame
+#     st_profile_report(profile_df)  # Displaying the profile report in the app
+
+# if choise == "Modelling":
+#    st.title("Machine Learning go")  # Setting the title for the modeling section
+#    st.write("Read Documentation ---> https://pycaret.org/")  # Providing a link to documentation
+#    chosen_target = st.selectbox('Choose the Target Column', df.columns)  # Creating a selectbox widget for choosing the target column
+#    if st.button('Run Modelling'):  # Creating a button widget for running modeling
+#       setup(df, target=chosen_target)  # Setting up the PyCaret environment for modeling
+#       setup_df = pull()  # Pulling the processed data after setup
+#       st.dataframe(setup_df)  # Displaying the processed data in the app
+#       best_model = compare_models()  # Comparing models and selecting the best one
+#       compare_df = pull()  # Pulling the comparison results
+#       st.dataframe(compare_df)  # Displaying the comparison results in the app
+#       save_model(best_model, 'best_model')  # Saving the best model
+
+# if choise == "Download":
+#    with open('best_model.pkl','rb') as f:  # Opening the saved model file in binary mode
+#         st.download_button('Download Model',f,file_name="best_model.pkl")  # Creating a download button for the model
+
+
